936a111ac48ec827a6c9b0717b9197569b52722a
gal <gal.oiring@gmail.com>
2023-09-20 12:34:16 +0300

    Footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import React from "react";
 
 const App = () => {
   return (
-    <h1>Wellcome to ProShop</h1>
-  )
-}
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Wellcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
