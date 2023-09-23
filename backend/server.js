62be59ea519936e8e38db3981d86e0072c3845a3
gal <gal.oiring@gmail.com>
2023-09-23 20:49:28 +0300

    mongo DB added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
