0df3006f776451b51e82df6186a4fbfab61d1904
gal <gal.oiring@gmail.com>
2023-10-01 15:19:54 +0300

    add CORS

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import dotenv from "dotenv";
+import cors from "cors"; // Import the cors middleware
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -10,6 +11,8 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+app.use(cors()); // Use the cors middleware to allow all origins during development
 
 app.get("/", (req, res) => {
   res.send("API is running ...");
@@ -20,4 +23,6 @@ app.use("/api/products", productRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.listen(port, "0.0.0.0", () =>
+  console.log(`Server is running on port ${port}`)
+);
