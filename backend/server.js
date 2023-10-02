ee654c3b98a0aaeebb55de9ed4aef2a75c1defba
gal <gal.oiring@gmail.com>
2023-10-02 19:40:19 +0300

    added authentication using bcrypt

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
 import cors from "cors"; // Import the cors middleware
 dotenv.config();
@@ -11,6 +12,9 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors()); // Use the cors middleware to allow all origins during development
 
@@ -19,6 +23,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
