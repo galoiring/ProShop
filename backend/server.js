1a53b1b0aab05ea09bbed4ac8d6ee9a01129d493
gal <gal.oiring@gmail.com>
2023-10-02 23:47:57 +0300

    add auth middleware and protected route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import cors from "cors"; // Import the cors middleware
 dotenv.config();
 
@@ -12,9 +13,13 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.use(cors()); // Use the cors middleware to allow all origins during development
 
