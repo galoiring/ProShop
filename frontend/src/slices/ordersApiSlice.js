6797c346ff6ed8c7938fab67a69ae2707faba7e8
gal <gal.oiring@gmail.com>
2023-10-05 14:54:33 +0300

    order screen created

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -6,11 +6,20 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
     createOrder: builder.mutation({
       query: (order) => ({
         url: ORDERS_URL,
-        method: POST,
+        method: "POST",
         body: { ...order },
       }),
+    }),
+    getOrderDetails: builder.query({
+      query: (orderId) => ({
+        url: `${ORDERS_URL}/${orderId}`,
+      }),
+      keepUnusedDataFor: 5,
     }),
   }),
 });
 
-export const { useCreateOrderMutation } = ordersApiSlice;
+export const {
+  useCreateOrderMutation,
+  useGetOrderDetailsQuery,
+} = ordersApiSlice;
