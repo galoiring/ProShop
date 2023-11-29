bc8ebf99b7522fd2d4bce91715f11720263a177b
gal <gal.oiring@gmail.com>
2023-11-29 17:54:58 +0200

    bug fixed

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -43,6 +43,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         url: `${PRODUCTS_URL}/${productId}`,
         method: "DELETE",
       }),
+      providesTags: ["Product"],
     }),
   }),
 });
